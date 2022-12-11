e105b254cfef2e8f52c6966af46f0a997544cdd3
Outchase <101480664+Outchase@users.noreply.github.com>
2022-12-11 21:15:29 +0100

    Add Advance setting

--- 3DCharacterControl/Assets/Scripts/AdvancedCharacterController.cs
+++ 3DCharacterControl/Assets/Scripts/AdvancedCharacterController.cs
@@ -8,6 +8,7 @@ public class AdvancedCharacterController : MonoBehaviour
     private Vector3 movementDirection;
     private Rigidbody rb;
     private float horizontalVelocity = 0;
+    private float verticalVelocity = 0;
 
     [SerializeField] float acceleration = 1;
     [SerializeField] float dampingMovingForward = 0.8f;
@@ -31,20 +32,23 @@ public class AdvancedCharacterController : MonoBehaviour
     {
         //rb.velocity += movementDirection * Time.deltaTime * speed;
         Movement();
-        rb.velocity = new Vector3(horizontalVelocity, rb.velocity.y, rb.velocity.z);
-
+        rb.velocity = new Vector3(horizontalVelocity, rb.velocity.y, verticalVelocity);
+        Debug.Log(movementDirection);
     }
 
     public void Movement()
     {
         //add basic velocity each frame
         horizontalVelocity = rb.velocity.x;
+        verticalVelocity = rb.velocity.z;
+
         horizontalVelocity += movementDirection.x;
-        
+        verticalVelocity += movementDirection.z;
+
         //flip negaive number into positive, verify movement direction and on 0 damp speed
         //damp speed when direction is switched
         //damp while moving forward
-        if (Mathf.Abs(movementDirection.x) < 0.01f) //Âbs flips negative to positive
+        if (Mathf.Abs(movementDirection.x) < 0.01f) //Abs flips negative to positive
         {
             horizontalVelocity *= Mathf.Pow(acceleration - dampingWhenStopping, Time.deltaTime * 10f);
         }
@@ -56,7 +60,20 @@ public class AdvancedCharacterController : MonoBehaviour
         {
             horizontalVelocity *= Mathf.Pow(acceleration - dampingMovingForward, Time.deltaTime * 10f);//x^y
         }
-        Debug.Log(horizontalVelocity);
 
+        //reformulate movement
+
+        if (Mathf.Abs(movementDirection.z) < 0.01f) //Abs flips negative to positive
+        {
+            verticalVelocity *= Mathf.Pow(acceleration - dampingWhenStopping, Time.deltaTime * 10f);
+        }
+        else if (Mathf.Sign(movementDirection.z) != Mathf.Sign(verticalVelocity))// Sign turns positive and 0 to 1 and negative to -1
+        {
+            verticalVelocity *= Mathf.Pow(acceleration - dampingWhenTurning, Time.deltaTime * 10f);
+        }
+        else
+        {
+            verticalVelocity *= Mathf.Pow(acceleration - dampingMovingForward, Time.deltaTime * 10f);//x^y
+        }
     }
 }
